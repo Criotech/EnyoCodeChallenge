03432195ea9a3e6aa84738f49ace4c3ff4e1dd79
Criotech <opesiyanbola8991@gmail.com>
2019-10-22 16:47:02 +0100

    feature-opeoluwa-add-redux

--- src/components/userForm.js
+++ src/components/userForm.js
@@ -1,90 +1,80 @@
-import React, { Component } from 'react'
-import UserList from './userList'
+import React from 'react';
+import UserList from './userList';
 import { Modal, Button, Input } from 'antd';
-import { connect } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import { userInputForm, submitForm, deleteUser, editUser, toggleModal } from '../actions';
 
-class userForm extends Component {
-
-    showModal = () => {
-        this.props.toggleModal()
-    };
-
-    handleCancel = () => {
-        this.props.toggleModal()
-    };
-
-
-    handleChange(event) {
-        const { name, value } = event.target
-
-        this.props.userInputForm({ prop: name, value })
-    }
-
+function UserForm() {
+    const dispatch = useDispatch()
+    const { firstName, lastName, age, birthday, hobby, id, userData, edit, visible } = useSelector(state => state.user)
     
-    deleteUser(data) {
-        this.props.deleteUser(data)
+    const showModal = data => {
+        dispatch(toggleModal() )       
+    }
+
+    const handleCancel = data => {
+        dispatch(toggleModal() )       
+    }
+
+    const handleChange = event => {
+        const { name, value } = event.target
+        console.log(name, value)
+        dispatch(userInputForm({ prop: name, value }))                
+    }
+    
+    const DeleteUser = data => {
+        dispatch(deleteUser(data) )       
     }
 
 
-    onSubmit(event) {
+    const onSubmit = event => {
         event.preventDefault()
         
-        if (this.props.edit===true){
-            this.deleteUser(this.props.id)
+        if (edit===true){
+            DeleteUser(id)
         }
-        const { id, firstName, lastName, age, birthday, hobby } = this.props;        
-        this.props.submitForm({ id, firstName, lastName, age, birthday, hobby })
 
+        dispatch(submitForm({ id, firstName, lastName, age, birthday, hobby }))                
     }
 
-    editUser(data) {
-        this.props.editUser(data)
+    const EditUser = data => {
+        dispatch(editUser(data) )                
     }
 
-    render() {
-        const { firstName, lastName, age, birthday, hobby, userData, edit, visible } = this.props;        
         return (
             <div>
-                <UserList showModal={this.showModal.bind(this)} editUser={this.editUser.bind(this)} deleteUser={this.deleteUser.bind(this)} entries={userData} />
+                 <UserList showModal={showModal} editUser={EditUser} deleteUser={DeleteUser} entries={userData} />
 
                 <div>
-                    <Button className="toggleForm" onClick={this.showModal}>
+                    <Button className="toggleForm" onClick={showModal}>
                         <i className="fa fa-plus" aria-hidden="true"></i>
                     </Button>
                     <Modal
                         visible={visible}
                         title="User Data Form"
-                        onCancel={this.handleCancel}
+                        onCancel={handleCancel}
                         footer={[
-                            <Button key="back" onClick={this.handleCancel}>
+                            <Button key="back" onClick={handleCancel}>
                                 Return
                             </Button>
                         ]}
                     >
                         <form>
-                            <Input type="text" name="firstName" value={firstName} onChange={this.handleChange.bind(this)} placeholder="first name" /> <br /> <br/>
-                            <Input type="text" name="lastName" value={lastName} onChange={this.handleChange.bind(this)} placeholder="last name" /> <br /> <br/>
-                            <Input type="number" name="age" value={age} onChange={this.handleChange.bind(this)} placeholder="age" /> <br /> <br/>
-                            <Input type="date" name="birthday" value={birthday} onChange={this.handleChange.bind(this)} placeholder="birthday" /> <br /> <br/>
-                            <Input name="hobby" value={hobby}  onChange={this.handleChange.bind(this)} type="text" placeholder="Hobby" /> <br /> <br/>
-                            <Input type="submit" className="inputButton" onClick={this.onSubmit.bind(this)} value={(edit===false)? 'Add': 'Edit'} />
+                            <Input type="text" name="firstName" value={firstName} onChange={handleChange} placeholder="first name" /> <br /> <br/>
+                            <Input type="text" name="lastName" value={lastName} onChange={handleChange} placeholder="last name" /> <br /> <br/>
+                            <Input type="number" name="age" value={age} onChange={handleChange} placeholder="age" /> <br /> <br/>
+                            <Input type="date" name="birthday" value={birthday} onChange={handleChange} placeholder="birthday" /> <br /> <br/>
+                            <Input name="hobby" value={hobby}  onChange={handleChange} type="text" placeholder="Hobby" /> <br /> <br/>
+                            <Input type="submit" className="inputButton" onClick={onSubmit} value={(edit===false)? 'Add': 'Edit'} />
 
                         </form>
                     </Modal>
-                </div>
+                </div> 
 
             </div>
         );
-    }
 }
 
+export default UserForm;
 
-const mapStateToProps = (state) => {
-    const { firstName, lastName, age, birthday, hobby, id, userData, edit, visible } = state.user;
-
-    return { firstName, lastName, age, birthday, hobby, id, userData, edit, visible}
-}
-
-export default connect(mapStateToProps, { userInputForm, submitForm, deleteUser, editUser, toggleModal }) (userForm)
 
--- src/components/userList.js
+++ src/components/userList.js
@@ -1,52 +1,50 @@
-import React, { Component } from 'react'
+import React from 'react';
 import { Table, Divider } from 'antd';
 
 const { Column, ColumnGroup } = Table;
 
-class userList extends Component {
-
-    EditUserData(data) {
-        this.props.showModal();
-        this.props.editUser(data)
+function UserList(props) {
+    let data
+    if (props.entries) {
+        data = props.entries
+    } else {
+        console.log('no entries')
     }
 
-    deleteUser(data) {
-        this.props.deleteUser(data)
+    const EditUserData = data => {
+        props.showModal();
+        props.editUser(data)
     }
 
-    render() {
-        let data
-        if (this.props.entries) {
-            data= this.props.entries
-        } else {
-            console.log('no entries')
-        }
-        return (
-            <div>
-                    <Table rowKey={data => data.id} dataSource={data}>
-                        <ColumnGroup title="Name">
-                            <Column title="First Name" dataIndex="firstName" key="firstName" />
-                            <Column title="Last Name" dataIndex="lastName" key="lastName" />
-                        </ColumnGroup>
-                        <Column title="Age" dataIndex="age" key="age" />
-                        <Column title="Birthday" dataIndex="birthday" key="birthday" />
-                        <Column title="Hobby" dataIndex="hobby" key="hobby" />
-                        
-                        <Column
-                            title="Action"
-                            key="action"
-                            render={(text, record) => (
-                                <span>
-                                    <span style={{color: "#3AA5FF", cursor: "pointer"}} onClick={this.EditUserData.bind(this, record)}>Edit </span>
-                                    <Divider type="vertical" />
-                                    <span onClick={this.deleteUser.bind(this, record.id)} style={{color: '#3AA5FF', cursor: "pointer"}}>Delete</span>
-                                </span>
-                            )}
-                        />
-                    </Table>
-            </div>
-        )
+    const deleteUser = data => {
+        props.deleteUser(data)
     }
+
+    return (
+        <div>
+             <Table rowKey={data => data.id} dataSource={data}>
+                <ColumnGroup title="Name">
+                    <Column title="First Name" dataIndex="firstName" key="firstName" />
+                    <Column title="Last Name" dataIndex="lastName" key="lastName" />
+                </ColumnGroup>
+                <Column title="Age" dataIndex="age" key="age" />
+                <Column title="Birthday" dataIndex="birthday" key="birthday" />
+                <Column title="Hobby" dataIndex="hobby" key="hobby" />
+
+                <Column
+                    title="Action"
+                    key="action"
+                    render={(text, record) => (
+                        <span>
+                            <span style={{ color: "#3AA5FF", cursor: "pointer" }} onClick={()=>EditUserData(record)}>Edit </span>
+                            <Divider type="vertical" />
+                            <span onClick={()=>deleteUser(record.id)} style={{ color: '#3AA5FF', cursor: "pointer" }}>Delete</span>
+                        </span>
+                    )}
+                />
+            </Table> 
+        </div>
+    );
 }
 
-export default userList
+export default UserList;
